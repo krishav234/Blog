665b9f7ce6206c2700dee5eef405bfed61d13ac3
krishav234 <krishav234@gmail.com>
2024-01-01 17:24:25 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <title>Rishav</title>
+      <title>Rishav Kumar</title>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
